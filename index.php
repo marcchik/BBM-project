5fe6c1d3d5b4e39901a465574994ff875eeb92f0
marcchik <markstal2002@gmail.com>
2022-03-03 08:55:11 +0300

    new components and new page - about company

--- index.php
+++ index.php
@@ -3,120 +3,76 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Главная");
 ?>
     <main class="main">
-
-
     <section class="baner-home">
         <div class="container">
             <div class="baner-home__inner">
-                <div class="baner-home__slider">
-                    <a href="#" class="baner-home__btn btn-link open__modal">Оставить заявку</a>
 
-                        <div class="swiper-container baner-home-swiper">
-                            <div class="swiper-wrapper">
-                                <div class="swiper-slide">
-                                    <div class="baner-home__slider-wrap ">
-                                        <div class="baner-home__slider-wrap-img">
-                                            <img src="/bitrix/templates/bbm/img/baner-home-slid3.jpg" alt="">
-                                        </div>
-                                        <div class="baner-home__slider-wrap-text">
-                                            Аренда складских помещений
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="swiper-slide">
-                                    <div class="baner-home__slider-wrap">
-                                        <div class="baner-home__slider-wrap-img">
-                                            <img src="/bitrix/templates/bbm/img/baner-home-slid1.jpg" alt="">
-                                        </div>
-                                        <div class="baner-home__slider-wrap-text">
-                                            Стоянка для&nbsp;больше-груза
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="swiper-slide">
-                                    <div class="baner-home__slider-wrap">
-                                        <div class="baner-home__slider-wrap-img">
-                                            <img src="/bitrix/templates/bbm/img/baner-home-slid2.jpg" alt="">
-                                        </div>
-                                        <div class="baner-home__slider-wrap-text">
-                                            Аренда рефрижера-торов
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                </div>
+                <? $APPLICATION->IncludeComponent("bitrix:news.list", "slider-img", Array(),
+                    false
+                ); ?>
+
                 <div class="baner-home__subtitle mobile">
                     Компания БММ оказывает складские услуги более 20 лет
                 </div>
-
                 <!--Преимущества компании-->
-                <?$APPLICATION->IncludeComponent(
-	"bitrix:news.list", 
-	"advantages", 
-	array(
-		"COMPONENT_TEMPLATE" => "advantages",
-		"IBLOCK_TYPE" => "advantages",
-		"IBLOCK_ID" => "11",
-		"NEWS_COUNT" => "20",
-		"SORT_BY1" => "ACTIVE_FROM",
-		"SORT_ORDER1" => "DESC",
-		"SORT_BY2" => "SORT",
-		"SORT_ORDER2" => "ASC",
-		"FILTER_NAME" => "",
-		"FIELD_CODE" => array(
-			0 => "",
-			1 => "",
-		),
-		"PROPERTY_CODE" => array(
-			0 => "DESCRIPTION",
-			1 => "",
-		),
-		"CHECK_DATES" => "Y",
-		"DETAIL_URL" => "",
-		"AJAX_MODE" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"AJAX_OPTION_HISTORY" => "N",
-		"AJAX_OPTION_ADDITIONAL" => "",
-		"CACHE_TYPE" => "A",
-		"CACHE_TIME" => "36000000",
-		"CACHE_FILTER" => "N",
-		"CACHE_GROUPS" => "Y",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"SET_TITLE" => "Y",
-		"SET_BROWSER_TITLE" => "Y",
-		"SET_META_KEYWORDS" => "Y",
-		"SET_META_DESCRIPTION" => "Y",
-		"SET_LAST_MODIFIED" => "N",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
-		"ADD_SECTIONS_CHAIN" => "Y",
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"PARENT_SECTION" => "",
-		"PARENT_SECTION_CODE" => "",
-		"INCLUDE_SUBSECTIONS" => "Y",
-		"STRICT_SECTION_CHECK" => "N",
-		"DISPLAY_DATE" => "Y",
-		"DISPLAY_NAME" => "Y",
-		"DISPLAY_PICTURE" => "Y",
-		"DISPLAY_PREVIEW_TEXT" => "Y",
-		"PAGER_TEMPLATE" => ".default",
-		"DISPLAY_TOP_PAGER" => "N",
-		"DISPLAY_BOTTOM_PAGER" => "Y",
-		"PAGER_TITLE" => "Новости",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_DESC_NUMBERING" => "N",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-		"PAGER_SHOW_ALL" => "N",
-		"PAGER_BASE_LINK_ENABLE" => "N",
-		"SET_STATUS_404" => "N",
-		"SHOW_404" => "N",
-		"MESSAGE_404" => ""
-	),
-	false
-);?>
-
+                <? $APPLICATION->IncludeComponent(
+                    "bitrix:news.list",
+                    "advantages",
+                    Array(
+                        "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                        "ADD_SECTIONS_CHAIN" => "Y",
+                        "AJAX_MODE" => "N",
+                        "AJAX_OPTION_ADDITIONAL" => "",
+                        "AJAX_OPTION_HISTORY" => "N",
+                        "AJAX_OPTION_JUMP" => "N",
+                        "AJAX_OPTION_STYLE" => "Y",
+                        "CACHE_FILTER" => "N",
+                        "CACHE_GROUPS" => "Y",
+                        "CACHE_TIME" => "36000000",
+                        "CACHE_TYPE" => "A",
+                        "CHECK_DATES" => "Y",
+                        "COMPONENT_TEMPLATE" => "advantages",
+                        "DETAIL_URL" => "",
+                        "DISPLAY_BOTTOM_PAGER" => "Y",
+                        "DISPLAY_DATE" => "Y",
+                        "DISPLAY_NAME" => "Y",
+                        "DISPLAY_PICTURE" => "Y",
+                        "DISPLAY_PREVIEW_TEXT" => "Y",
+                        "DISPLAY_TOP_PAGER" => "N",
+                        "FIELD_CODE" => array(0 => "", 1 => "",),
+                        "FILTER_NAME" => "",
+                        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                        "IBLOCK_ID" => "11",
+                        "IBLOCK_TYPE" => "advantages",
+                        "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+                        "INCLUDE_SUBSECTIONS" => "Y",
+                        "MESSAGE_404" => "",
+                        "NEWS_COUNT" => "20",
+                        "PAGER_BASE_LINK_ENABLE" => "N",
+                        "PAGER_DESC_NUMBERING" => "N",
+                        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                        "PAGER_SHOW_ALL" => "N",
+                        "PAGER_SHOW_ALWAYS" => "N",
+                        "PAGER_TEMPLATE" => ".default",
+                        "PAGER_TITLE" => "Новости",
+                        "PARENT_SECTION" => "",
+                        "PARENT_SECTION_CODE" => "",
+                        "PREVIEW_TRUNCATE_LEN" => "",
+                        "PROPERTY_CODE" => array(0 => "DESCRIPTION", 1 => "",),
+                        "SET_BROWSER_TITLE" => "Y",
+                        "SET_LAST_MODIFIED" => "N",
+                        "SET_META_DESCRIPTION" => "Y",
+                        "SET_META_KEYWORDS" => "Y",
+                        "SET_STATUS_404" => "N",
+                        "SET_TITLE" => "Y",
+                        "SHOW_404" => "N",
+                        "SORT_BY1" => "ACTIVE_FROM",
+                        "SORT_BY2" => "SORT",
+                        "SORT_ORDER1" => "DESC",
+                        "SORT_ORDER2" => "ASC",
+                        "STRICT_SECTION_CHECK" => "N"
+                    )
+                ); ?>
             </div>
             <div class="baner-home-text">
                 Сдача в аренду коммерческих площадей
@@ -163,6 +119,7 @@ $APPLICATION->SetTitle("Главная");
             "CACHE_TIME" => "3600",
             "CACHE_TYPE" => "A",
             "CHECK_DATES" => "Y",
+            "COMPONENT_TEMPLATE" => "services",
             "DETAIL_URL" => "",
             "DISPLAY_BOTTOM_PAGER" => "Y",
             "DISPLAY_DATE" => "Y",
@@ -170,7 +127,8 @@ $APPLICATION->SetTitle("Главная");
             "DISPLAY_PICTURE" => "Y",
             "DISPLAY_PREVIEW_TEXT" => "Y",
             "DISPLAY_TOP_PAGER" => "Y",
-            "FIELD_CODE" => Array("ID"),
+            "FIELD_CODE" => array(0 => "ID", 1 => "",),
+            "FILE_404" => "",
             "FILTER_NAME" => "",
             "HIDE_LINK_WHEN_NO_DETAIL" => "Y",
             "IBLOCK_ID" => "10",
@@ -191,7 +149,7 @@ $APPLICATION->SetTitle("Главная");
             "PARENT_SECTION" => "",
             "PARENT_SECTION_CODE" => "",
             "PREVIEW_TRUNCATE_LEN" => "",
-            "PROPERTY_CODE" => Array("DESCRIPTION"),
+            "PROPERTY_CODE" => array(0 => "DESCRIPTION", 1 => "",),
             "SET_BROWSER_TITLE" => "Y",
             "SET_LAST_MODIFIED" => "Y",
             "SET_META_DESCRIPTION" => "Y",
@@ -199,10 +157,11 @@ $APPLICATION->SetTitle("Главная");
             "SET_STATUS_404" => "Y",
             "SET_TITLE" => "Y",
             "SHOW_404" => "Y",
-            "SORT_BY1" => "ACTIVE_FROM",
+            "SORT_BY1" => "ID",
             "SORT_BY2" => "SORT",
-            "SORT_ORDER1" => "DESC",
-            "SORT_ORDER2" => "ASC"
+            "SORT_ORDER1" => "ASC",
+            "SORT_ORDER2" => "ASC",
+            "STRICT_SECTION_CHECK" => "N"
         )
     ); ?>
     <section class="feedback">
