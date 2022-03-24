954b973bffd59d5019ac9ebe79c40d993fd5ba70
marcchik <markstal2002@gmail.com>
2022-03-24 10:28:09 +0300

    refactor: start servicies page

---
+++ services/index.php
@@ -0,0 +1,208 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Наш АСЦ");
+?>
+
+<div class="pageContent services">
+    <section class="bestPrices bgGray">
+        <div class="bpContentWrapper">
+            <div class="left">
+                <div class="owl-carousel servicesSlider">
+                    <div class="item">
+                        <div class="info">
+                            <a href="" class="verticalButton mobHide">
+                                <span>Scroll to catalog</span>
+                                <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/scroll.svg" alt="">
+                            </a>
+                            <div class="title">Сервисный центр ESC</div>
+                            <div class="textGray">
+                                <p>
+                                    ESC – это федеральная сеть Сервисных Центров, обслуживающих ноутбуки, планшеты,
+                                    ПК, печатное оборудование, серверы, СХД, сетевое оборудование и источники бесперебойного питания.
+                                </p>
+                                <p>Миссия компании - предоставлять наилучший сервис     для своих клиентов.</p>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <span class="iconBlueButton">&rarr;</span>
+                                    <span>Записаться на ремонт</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="imgWrapper"><img src="<?=SITE_TEMPLATE_PATH?>/img/sliderItem1.png" alt=""></div>
+                    </div>
+                    <div class="item">
+                        <div class="info">
+                            <a href="" class="verticalButton mobHide">
+                                <span>Scroll to catalog</span>
+                                <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/scroll.svg" alt="">
+                            </a>
+                            <div class="title">Сервисный центр ESC</div>
+                            <div class="textGray">
+                                <p>
+                                    ESC – это федеральная сеть Сервисных Центров, обслуживающих ноутбуки, планшеты,
+                                    ПК, печатное оборудование, серверы, СХД, сетевое оборудование и источники бесперебойного питания.
+                                </p>
+                                <p>Миссия компании - предоставлять наилучший сервис     для своих клиентов.</p>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <span class="iconBlueButton">&rarr;</span>
+                                    <span>Записаться на ремонт</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="imgWrapper"><img src="<?=SITE_TEMPLATE_PATH?>/img/sliderItem1.png" alt=""></div>
+                    </div>
+                    <div class="item">
+                        <div class="info">
+                            <a href="" class="verticalButton mobHide">
+                                <span>Scroll to catalog</span>
+                                <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/scroll.svg" alt="">
+                            </a>
+                            <div class="title">Сервисный центр ESC</div>
+                            <div class="textGray">
+                                <p>
+                                    ESC – это федеральная сеть Сервисных Центров, обслуживающих ноутбуки, планшеты,
+                                    ПК, печатное оборудование, серверы, СХД, сетевое оборудование и источники бесперебойного питания.
+                                </p>
+                                <p>Миссия компании - предоставлять наилучший сервис     для своих клиентов.</p>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <span class="iconBlueButton">&rarr;</span>
+                                    <span>Записаться на ремонт</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="imgWrapper"><img src="<?=SITE_TEMPLATE_PATH?>/img/sliderItem1.png" alt=""></div>
+                    </div>
+                    <div class="item">
+                        <div class="info">
+                            <a href="" class="verticalButton mobHide">
+                                <span>Scroll to catalog</span>
+                                <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/scroll.svg" alt="">
+                            </a>
+                            <div class="title">Сервисный центр ESC</div>
+                            <div class="textGray">
+                                <p>
+                                    ESC – это федеральная сеть Сервисных Центров, обслуживающих ноутбуки, планшеты,
+                                    ПК, печатное оборудование, серверы, СХД, сетевое оборудование и источники бесперебойного питания.
+                                </p>
+                                <p>Миссия компании - предоставлять наилучший сервис     для своих клиентов.</p>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <span class="iconBlueButton">&rarr;</span>
+                                    <span>Записаться на ремонт</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="imgWrapper"><img src="<?=SITE_TEMPLATE_PATH?>/img/sliderItem1.png" alt=""></div>
+                    </div>
+                    <div class="item">
+                        <div class="info">
+                            <a href="" class="verticalButton mobHide">
+                                <span>Scroll to catalog</span>
+                                <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/scroll.svg" alt="">
+                            </a>
+                            <div class="title">Сервисный центр ESC</div>
+                            <div class="textGray">
+                                <p>
+                                    ESC – это федеральная сеть Сервисных Центров, обслуживающих ноутбуки, планшеты,
+                                    ПК, печатное оборудование, серверы, СХД, сетевое оборудование и источники бесперебойного питания.
+                                </p>
+                                <p>Миссия компании - предоставлять наилучший сервис     для своих клиентов.</p>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <span class="iconBlueButton">&rarr;</span>
+                                    <span>Записаться на ремонт</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="imgWrapper"><img src="<?=SITE_TEMPLATE_PATH?>/img/sliderItem1.png" alt=""></div>
+                    </div>
+                </div>
+                <div class="sliderNav">
+                    <div class="custom-owl-prev">
+                        <svg class="iconLongArrow" viewBox="0 0 106 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M94 1L104.5 11.5M104.5 11.5L94 22M104.5 11.5H0" stroke="none" stroke-width="1.5"/>
+                        </svg>
+                    </div>
+                    <div class="custom-owl-next">
+                        <svg class="iconLongArrow" viewBox="0 0 106 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M94 1L104.5 11.5M104.5 11.5L94 22M104.5 11.5H0" stroke="none" stroke-width="1.5"/>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="aboutUs">
+        <div class="contentWrapper">
+            <div class="info">
+                <div class="title">Интересное о нас</div>
+                <p>С компанией ESC сотрудничают крупнейшие мировые производители, такие как: Dell, HP, Zyxel, Aten, Impulse,
+                    Makelsan, а так же крупнейшие Российские ИТ-компании.</p>
+                <p>Инженеры ESC постоянно проходят специализированное обучение, сдают международные экзамены, получают
+                    сертификаты, подтверждающие их квалификацию.</p>
+                <div>
+                    <a href="">
+                        <span class="iconBlueButton">→</span>
+                        <span>Связаться с нами</span>
+                    </a>
+                </div>
+            </div>
+            <div class="imgWrap">
+                <img src="<?=SITE_TEMPLATE_PATH?>/img/about.png" alt="">
+            </div>
+        </div>
+    </section>
+    <section class="ourContacts2">
+        <div class="contentWrapper">
+            <div class="info">
+                <div class="contentTitle2">Наши контакты</div>
+                <div class="textGray">Вы можете связаться с нашим<br class="mobShow"> онлайн-консультантом</div>
+                <ul class="contactsList">
+                    <li>
+                        <div class="title"><span class="icon"><img src="<?=SITE_TEMPLATE_PATH?>/img/icons/location.svg" alt=""></span>Адрес</div>
+                        <p>Москва, ул. 3-я Хорошевская, дом 2, строение 1</p>
+                    </li>
+                    <li>
+                        <div class="title"><span class="icon"><img src="<?=SITE_TEMPLATE_PATH?>/img/icons/call-calling.svg" alt=""></span>Телефон</div>
+                        <p>+7 495 737-06-01<br>+7 495 737-06-01</p>
+                    </li>
+                    <li>
+                        <div class="title"><span class="icon"><img src="<?=SITE_TEMPLATE_PATH?>/img/icons/www.svg" alt=""></span>Сайт</div>
+                        <p>
+                            <a href="www.dell-dell.ru">dell-dell.ru</a><br>
+                            <a href="www.dell-russia-dell.ru">dell-russia-dell.ru</a>
+                        </p>
+                    </li>
+                    <li>
+                        <div class="title"><span class="icon"><img src="<?=SITE_TEMPLATE_PATH?>/img/icons/sms.svg" alt=""></span>E-mail</div>
+                        <p><a href="mailto:dell_ru@gmail.com">dell_ru@gmail.com</a></p>
+                    </li>
+                </ul>
+                <div class="bgC2">
+                    <img src="<?=SITE_TEMPLATE_PATH?>/img/contacts2.png" alt="">
+                </div>
+                <div class="bgLineOc"></div>
+            </div>
+        </div>
+    </section>
+    <section class="yaMap">
+        <div class="yaMapTitle">
+            <svg viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M18.9996 21.264C21.7279 21.264 23.9396 19.0523 23.9396 16.324C23.9396 13.5957 21.7279 11.384 18.9996 11.384C16.2713 11.384 14.0596 13.5957 14.0596 16.324C14.0596 19.0523 16.2713 21.264 18.9996 21.264Z" stroke="white" stroke-width="3"/>
+                <path d="M5.73202 13.4426C8.85119 -0.269081 29.1654 -0.253248 32.2687 13.4584C34.0895 21.5017 29.0862 28.3101 24.7004 32.5217C21.5179 35.5934 16.4829 35.5934 13.2845 32.5217C8.91452 28.3101 3.91119 21.4859 5.73202 13.4426Z" stroke="white" stroke-width="3"/>
+            </svg>
+            <span>Основной Офис: г. Москва, Солнечногорский проезд, д. 11</span>
+            <span>Телефон: 8 800 555-31-52</span>
+        </div>
+        <div id="yaMap"></div>
+    </section>
+</div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
